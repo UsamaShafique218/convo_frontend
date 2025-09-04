ed99e2722ada4b196f81767ef5788fb506e671d4
UsamaShafique218 <usamashafique218@gmail.com>
2025-09-04 18:14:55 +0500

    fix the profile issue

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import "./custom_responsive.css";
 
 import { SignIn, SignUp } from "./pages/auth";   
 import ProtectedRoute from "./pages/components/ProtectedRoute"; 
-import Profile from "@/pages/dashboard/Profile";
+import Profile from "./pages/dashboard/Profile";
 
 function App() {
   return (
